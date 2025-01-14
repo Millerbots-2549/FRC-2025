907135f4463639d16a4eea24a91fa5ffc04ed5b5
Linus Honer <lhon2701@mpsedu.org>
2025-01-13 19:25:27 -0600

    Gyro Simulation Added

---
+++ FRC2025/src/main/java/frc/robot/util/SimulationUtils.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.util;
+
+import static edu.wpi.first.units.Units.Seconds;
+
+import org.ironmaple.simulation.SimulatedArena;
+
+import edu.wpi.first.wpilibj.Timer;
+
+/** Add your docs here. */
+public class SimulationUtils {
+    public static double[] getSimulationOdometryTimeStamps() {
+        final double[] odometryTimeStamps = new double[SimulatedArena.getSimulationSubTicksIn1Period()];
+        for (int i = 0; i < odometryTimeStamps.length; i++) {
+        odometryTimeStamps[i] =
+            Timer.getFPGATimestamp() - 0.02 + i * SimulatedArena.getSimulationDt().in(Seconds);
+        }
+
+        return odometryTimeStamps;
+    }
+}
