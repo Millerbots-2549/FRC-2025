eece9fcbab8fd88c73b7b6658e290bdc2482168d
Linus Honer <lhon2701@mpsedu.org>
2025-01-13 23:05:47 -0600

    Basic PhotonVision Stuff

--- FRC2025/src/main/java/frc/robot/util/vision/QuestNav.java
+++ FRC2025/src/main/java/frc/robot/util/vision/QuestNav.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.util.vision;
+package frc.robot.util.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Quaternion;
