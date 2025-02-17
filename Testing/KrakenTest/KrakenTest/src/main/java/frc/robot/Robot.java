c5be4e3e1ea781566841dca39310567eecbd0b00
Lingus Honer <lhon2701@mpsedu.org>
2025-02-17 13:40:25 -0600

    Kraken Testing

---
+++ Testing/KrakenTest/KrakenTest/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,101 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ * This sample program shows how to control a motor using a joystick. In the operator control part
+ * of the program, the joystick is read and the value is written to the motor.
+ *
+ * <p>Joystick analog values range from -1 to 1 and motor controller inputs also range from -1 to 1
+ * making it easy to work together.
+ *
+ * <p>In addition, the encoder value of an encoder connected to ports 0 and 1 is consistently sent
+ * to the Dashboard.
+ */
+public class Robot extends TimedRobot {
+  private final TalonFX m_fx = new TalonFX(2);
+
+  /* Be able to switch which control request to use based on a button press */
+  /* Start at position 0, use slot 0 */
+  private final PositionVoltage m_positionVoltage = new PositionVoltage(0).withSlot(0);
+  /* Start at position 0, use slot 1 */
+  private final PositionTorqueCurrentFOC m_positionTorque = new PositionTorqueCurrentFOC(0).withSlot(1);
+  /* Keep a brake request so we can disable the motor */
+  private final NeutralOut m_brake = new NeutralOut();
+
+  private final XboxController m_joystick = new XboxController(0);
+
+  /** Called once at the beginning of the robot program. */
+  public Robot() {
+    TalonFXConfiguration configs = new TalonFXConfiguration();
+    configs.Slot0.kP = 2.4; // An error of 1 rotation results in 2.4 V output
+    configs.Slot0.kI = 0; // No output for integrated error
+    configs.Slot0.kD = 0.1; // A velocity of 1 rps results in 0.1 V output
+    // Peak output of 8 V
+    configs.Voltage.withPeakForwardVoltage(Volts.of(8))
+      .withPeakReverseVoltage(Volts.of(-8));
+
+    configs.Slot1.kP = 60; // An error of 1 rotation results in 60 A output
+    configs.Slot1.kI = 0; // No output for integrated error
+    configs.Slot1.kD = 6; // A velocity of 1 rps results in 6 A output
+    // Peak output of 120 A
+    configs.TorqueCurrent.withPeakForwardTorqueCurrent(Amps.of(120))
+      .withPeakReverseTorqueCurrent(Amps.of(-120));
+
+    /* Retry config apply up to 5 times, report if failure */
+    StatusCode status = StatusCode.StatusCodeNotInitialized;
+    for (int i = 0; i < 5; ++i) {
+      status = m_fx.getConfigurator().apply(configs);
+      if (status.isOK()) break;
+    }
+    if (!status.isOK()) {
+      System.out.println("Could not apply configs, error code: " + status.toString());
+    }
+
+    m_fx.setPosition(0.0);
+  }
+
+  /*
+   * The RobotPeriodic function is called every control packet no matter the
+   * robot mode.
+   */
+  @Override
+  public void robotPeriodic() {
+    
+  }
+
+  /** The teleop periodic function is called every control packet in teleop. */
+  @Override
+  public void teleopPeriodic() {
+    double desiredRotations = m_joystick.getLeftY() * 10; // Go for plus/minus 10 rotations
+    if (Math.abs(desiredRotations) <= 0.1) { // Joystick deadzone
+      desiredRotations = 0;
+    }
+
+    if (m_joystick.getLeftBumperButton()) {
+      /* Use position voltage */
+      m_fx.setControl(m_positionVoltage.withPosition(desiredRotations));
+    } else if (m_joystick.getRightBumperButton()) {
+      /* Use position torque */
+      m_fx.setControl(m_positionTorque.withPosition(desiredRotations));
+    } else {
+      /* Disable the motor instead */
+      m_fx.setControl(m_brake);
+    }
+  }
+}
