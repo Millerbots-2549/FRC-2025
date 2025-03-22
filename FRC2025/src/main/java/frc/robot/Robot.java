81940152848a88ae3495186929a04aacc0a1cbf8
Croix <ckos2801@mpsedu.org>
2025-03-22 13:39:40 -0500

    LED fix

--- FRC2025/src/main/java/frc/robot/Robot.java
+++ FRC2025/src/main/java/frc/robot/Robot.java
@@ -16,6 +16,7 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Constants.FieldConstants;
 
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
@@ -23,6 +24,9 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.util.Color;LEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.TimedRobot;
 
@@ -39,17 +43,10 @@ public class Robot extends LoggedRobot {
   private final AddressableLED m_led;
   private final AddressableLEDBuffer m_ledBuffer;
 
-  // Create an LED pattern that will display a rainbow across
-  // all hues at maximum saturation and half brightness
-  private final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
+  
 
-  // Our LED strip has a density of 120 LEDs per meter
-  private static final Distance kLedSpacing = Meters.of(1 / 60.0);
-
-  // Create a new pattern that scrolls the rainbow pattern across the LED strip, moving at a speed
-  // of 1 meter per second.
-  private final LEDPattern m_scrollingRainbow =
-      m_rainbow.scrollAtAbsoluteSpeed(MetersPerSecond.of(1), kLedSpacing);
+  
+  
 
 
 
@@ -62,17 +59,19 @@ public class Robot extends LoggedRobot {
     
      // PWM port 9
     // Must be a PWM header, not MXP or DIO
-    m_led = new AddressableLED(1);
+    m_led = new AddressableLED(0);
 
     // Reuse buffer
     // Default to a length of 60, start empty output
     // Length is expensive to set, so only set it once, then just update data
-    m_ledBuffer = new AddressableLEDBuffer(29);
+    m_ledBuffer = new AddressableLEDBuffer(27);
     m_led.setLength(m_ledBuffer.getLength());
 
     // Set the data
     m_led.setData(m_ledBuffer);
     m_led.start();
+  
+    
 
     
     Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
@@ -129,9 +128,12 @@ public class Robot extends LoggedRobot {
   public void robotPeriodic() {
     Threads.setCurrentThreadPriority(true, 99);
 
-    // Update the buffer with the rainbow animation
-    m_scrollingRainbow.applyTo(m_ledBuffer);
-    // Set the LEDs
+    LEDPattern red = LEDPattern.solid(Color.kRed);
+
+    // Apply the LED pattern to the data buffer
+    red.applyTo(m_ledBuffer);
+
+    // Write the data to the LED strip
     m_led.setData(m_ledBuffer);
 
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
